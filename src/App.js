c028eabd1e2c3f80f92f34e4e2030977234f6777
h29839 <hikari9672@gmail.com>
2023-04-20 15:01:33 +0900

    ルーティングの追加

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
+import { Router,Route } from "react-router-dom";
 import './App.css';
-import Header from './Header';
-import Threads from './Threads';
-import CreateThread from './CreateThread';
-
+import Home from "./Home";
+import Posts from "./Posts";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main className="App-body">
-        <Threads />
-        <CreateThread />
-      </main>
-    </div>
+    <Router>
+      <Route path="/" element={<Home />} />
+      <Route path=":threadId" element={<Posts />}/>
+    </Router>
+
   );
 }
 
